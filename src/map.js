95a2e20a0c36400b3abeba07ceeb8528b7a5e37e
ckb <ckingbailey@gmail.com>
2018-09-15 12:45:24 -0700

    the map is in there

--- src/map.js
+++ src/map.js
@@ -6,7 +6,17 @@ export default class Map extends Component {
         return (
             <section class="grid-x">
                 <div className="medium-10 medium-offset-1 map-container">
-                    <div className="map-container__map-placeholder"></div>
+                    <iframe
+                        src="https://arcgis-content.maps.arcgis.com/apps/opsdashboard/index.html#/78bf1715d46c431ca75930ea88ebb653"
+                        id="damMap"
+                        name="Embedded Dam Map"
+                        width="100%"
+                        height="900"
+                        frameborder="0"
+                        scrolling="no"
+                        marginheight="0"
+                        marginwidth="0"
+                    ></iframe>
                 </div>
             </section>
         )
