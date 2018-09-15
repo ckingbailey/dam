1bf3be59dc4af991c6ca102270de8c7241c3be7d
ckb <ckingbailey@gmail.com>
2018-09-14 21:35:28 -0700

    add an hr to hero

--- src/Hero.js
+++ src/Hero.js
@@ -6,15 +6,15 @@ class Hero extends React.Component {
 		return (
 			<div className="grid-x align-middle hero__image">
 				<div className="cell medium-6 medium-offset-3 hero__text">
-				<h1 className="hero__headline">Did you know:</h1>
+					<h1 className="hero__headline">Did you know</h1>
+					<hr className="hero__underline" />
 					<h2>
 						<ul>
-							<li>Dams can break</li>
-							<li>Broken dams are bad!</li>
-							<li>They can kill youuuuu...</li>
+							<li>...Dams can break</li>
+							<li>...Broken dams are bad!</li>
+							<li>...They can kill youuuuu!</li>
 						</ul>
 					</h2>
-					
 				</div>
 			</div>
 		);
