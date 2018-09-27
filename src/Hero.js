12208dceef74f0f56adba78e71f7b270880a0e88
ckb <ckingbailey@gmail.com>
2018-09-27 11:21:55 -0700

    change hero text to title, add story text below

--- src/Hero.js
+++ src/Hero.js
@@ -5,16 +5,7 @@ class Hero extends React.Component {
 		return (
 			<div className="grid-x align-middle hero__image">
 				<div className="cell medium-6 medium-offset-3 hero__text">
-					<h1 className="hero__headline">Did you know</h1>
-					<hr className="hero__underline" />
-					<h5>
-						<ul>
-							<li>There are over 1400 dams in California.</li>
-							<li>90% are considered to be safe by the Office of Dam Safety, but that still leaves 97 dams in questionable condition.</li>
-							<li>Out of these, 61 have high or extremely high downstream hazard.</li>
-							<li>See the map below to check if one of these dams is near you, and what you can do about it.</li>
-						</ul>
-					</h5>
+					<h1 className="hero__headline">Dam Assessment Mapping and Safety System</h1>
 				</div>
 			</div>
 		);
---
+++ src/story.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Story extends Component {
+    render() {
+        return (
+            <div className="story">
+                <ul>
+                    <li>There are almost 1400 dams in the State of California</li>
+                    <li>They are inspected annually by the DWR Division of Safety of Dams. Currently, 92% are in satisfactory condition. That leaves 98 dams that are not</li>
+                    <li>Only 5 of these dams are actually owned by the State. 44 are owned by city or county, and 49 are privately owned.</li>
+                    <li>Just because a dam has been assessed as unsafe does not mean money has been set aside to repair it.</li>
+                    <li>If one of these dams is near you, contact your Representative and local paper to ensure that there is a plan in place to bring it up to code!</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Story;
