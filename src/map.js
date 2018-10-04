d22551c57af7b545ff8b4d18e7881d45fc932456
ckb <ckingbailey@gmail.com>
2018-10-03 18:37:48 -0700

    fix DOM props on iframe so React stops complaining

--- src/map.js
+++ src/map.js
@@ -3,18 +3,19 @@ import React, { Component } from 'react'
 export default class Map extends Component {
     render() {
         return (
-            <section class="grid-x">
+            <section className="grid-x">
                 <div className="medium-10 medium-offset-1 map-container">
+                    <p></p>
                     <iframe
                         src="https://arcgis-content.maps.arcgis.com/apps/opsdashboard/index.html#/78bf1715d46c431ca75930ea88ebb653"
                         id="damMap"
                         title="Embedded Dam Map"
                         width="100%"
                         height="900"
-                        frameborder="0"
+                        frameBorder="0"
                         scrolling="no"
-                        marginheight="0"
-                        marginwidth="0"
+                        marginHeight="0"
+                        marginWidth="0"
                     ></iframe>
                 </div>
             </section>
