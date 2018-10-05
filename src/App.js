93aa484fee40783165cb4f7e5e3cedfddee9eca1
ckb <ckingbailey@gmail.com>
2018-10-04 17:56:15 -0700

    add AboutModal compo and fcn to show it. not wired up yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import AboutModal from './aboutModal.js'
 import Hero from './Hero.js'
 import Navbar from './navbar.js'
 import Map from './map.js'
 import Story from './story.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showAboutModal: false
+    }
+  }
+
+  maybeShowAboutModal() {
+    return this.state.showAboutModel ?
+      <AboutModal /> : null
+  }
+
+  toggleAboutModal() {
+    this.setState(prev => {
+      return { showAboutModal: !prev.showAboutModal }
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        { this.maybeShowAboutModal() }
         <Navbar />
         <Hero />
         <Story />
---
+++ src/aboutModal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class AboutModal extends Component {
+    render() {
+        return (
+            <div className="about-modal"></div>
+        )
+    }
+}
+
+export default AboutModal;
