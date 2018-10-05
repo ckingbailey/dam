0fc54fd721f5ecce037e9345f3e6b8ccb0814aff
ckb <ckingbailey@gmail.com>
2018-10-04 19:07:05 -0700

    add onclickoutside backdrop to About modal

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        { this.state.showAboutModal ? <div onClick={ this.closeAboutModal } className="about-modal__onclickoutside"></div> : null }
         { this.state.showAboutModal ? <AboutModal handleCloseClick={ () => this.closeAboutModal() } /> : null }
         <Navbar handleAboutClick={ () => this.toggleAboutModal() } />
         <Hero />
--- src/aboutModal.js
+++ src/aboutModal.js
@@ -4,7 +4,7 @@ class AboutModal extends Component {
     render() {
         const damReportLink = "https://water.ca.gov/-/media/DWR-Website/Web-Pages/Programs/All-Programs/Division-of-safety-of-dams/Files/Publications/Dams-Within-Jurisdiction-of-the-State-of-California-2018-Alphabetically-by-Dam-Name.pdf";
         return (
-            <div className="about-modal">
+            <div onClick={ () => null } className="about-modal">
                 <button onClick={ this.props.handleCloseClick } className="about-modal__close-button">&times;</button>
                 <p>This project was created for the <a href="http://waterchallenge.data.ca.gov/">California Water Data Challenge</a></p>
                 <p>It was inspired by the recent publication of the report <a href={ damReportLink } className="about-modal__repot-link">Dams Within Jurisdiction of the State of California</a></p>
