6db809ba4d5c1b12602900c852aa772329c6af9e
ckb <ckingbailey@gmail.com>
2018-10-05 12:37:43 -0700

    add photo and where we met to About

--- src/aboutModal.js
+++ src/aboutModal.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import teamPhoto from './assets/teamPhoto.jpg'
 
 class AboutModal extends Component {
     render() {
@@ -14,8 +15,11 @@ class AboutModal extends Component {
                 <button onClick={ this.props.handleCloseClick } className="about-modal__close-button">&times;</button>
                 <p>This project was created for the <a href="http://waterchallenge.data.ca.gov/">California Water Data Challenge</a></p>
                 <p>It was inspired by the recent publication of the report <a href={ damReportLink } className="about-modal__repot-link">Dams Within Jurisdiction of the State of California</a></p>
+                <p>We began this project in mid-September, 2018 at the CA Water Data Hackathon at the <a href="https://bids.berkeley.edu/">Berkeley Institute for Data Science</a></p>
                 <p>This is an open source project. You can find our development repo <a href="https://github.com/ckingbailey/dam">here</a></p>
                 <p className="about-modal__contributors">Project contributors: <a href={ contributorLinks.dan } target="_blank">Dan Constable</a>, <a href={ contributorLinks.colin } target="_blank">Colin King-Bailey</a>, <a href={ contributorLinks.jessica } target="_blank">Jessica Rahman</a>, <a href={ contributorLinks.daniel } target="_blank">Daniel Siegel</a></p>
+                <img src={ teamPhoto } alt="The team, the day we met at California Water Data Hackathon" aria-labelledby="team-pic-label" className="about-modal__team-pic" />
+                <aside id="team-pic-label" className="about-modal__team-pic-label">The team, from left to right: Colin King-Bailey, Jessica Rahman, Daniel Siegel, and Dan Constable</aside>
             </div>
         )
     }
