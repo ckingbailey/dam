4fe126a17d4ecd3092bf7d9f17a6af85ed8c7399
ckb <ckingbailey@gmail.com>
2018-10-04 18:32:58 -0700

    add content to About modal

--- src/aboutModal.js
+++ src/aboutModal.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react'
 
 class AboutModal extends Component {
     render() {
+        const damReportLink = "https://water.ca.gov/-/media/DWR-Website/Web-Pages/Programs/All-Programs/Division-of-safety-of-dams/Files/Publications/Dams-Within-Jurisdiction-of-the-State-of-California-2018-Alphabetically-by-Dam-Name.pdf";
         return (
-            <div className="about-modal">Hey!</div>
+            <div className="about-modal">
+                <p>This project was created for the <a href="http://waterchallenge.data.ca.gov/">California Water Data Challenge</a></p>
+                <p>It was inspired by the recent publication of the report <a href={ damReportLink } className="about-modal__repot-link">Dams Within Jurisdiction of the State of California</a></p>
+                <p>This is an open source project. You can find our development repo <a href="https://github.com/ckingbailey/dam">here</a></p>
+                <p className="about-modal__contributors">Project contributors: <a>Dan Constable</a>, <a>Colin King-Bailey</a>, <a>Jessica Rahman</a>, <a>Daniel Siegel</a></p>
+            </div>
         )
     }
 }
